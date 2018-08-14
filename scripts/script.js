083e41dcb9373e70ef68bc7478ef6133e954e457
Niket Patil <12906418+Niket03@users.noreply.github.com>
2018-08-14 17:20:48 +0530

    Changes in some file like test2 and script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,14 @@ var app = angular.module("myModule", []);
 app.controller("firstController", function($scope) {
 	$scope.message = "Angular demo";
 });
+
+
+app.controller("dataController", function($scope){
+
+		var listEmployees = [
+		{ firstName: "Pravin",lastName: "Malkar",gender: "Male", salary: 95000},
+		{ firstName: "Juhi", lastName: "Ichale",gender: "Female", salary: 65000},
+		{ firstName: "Aryan", lastName: "Patil",gender: "Male", salary: 98000}
+		];
+		$scope.employees = listEmployees;
+});
